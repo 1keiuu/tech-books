e9296d323b5152b23cb3fdc9d93e198a746f6813
1keiuu <ikkei12.h@gmail.com>
2021-04-21 22:15:42 +0900

    fix

--- app/scripts/updateReadme/templateUtility.js
+++ app/scripts/updateReadme/templateUtility.js
@@ -2,7 +2,7 @@ exports.createCheckBoxText = (obj, genre) => {
   let text = "";
   if (obj.isDone) text = "- [x]";
   else text = "- [ ]";
-  const noteLink = `tech-books/tree/main/notes/${genre}/${obj.slug}`;
+  const noteLink = `/tech-books/tree/main/notes/${genre}/${obj.slug}`;
   return `${text} [${obj.title}](${obj.amazonLink}) / [memo](${noteLink})`;
 };
 
