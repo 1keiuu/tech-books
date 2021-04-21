8e52df83361ce56adbe8dd75c50a499da9a67f75
1keiuu <ikkei12.h@gmail.com>
2021-04-21 19:24:19 +0900

    fix

--- app/scripts/updateReadme/index.js
+++ app/scripts/updateReadme/index.js
@@ -12,16 +12,18 @@ const templateStr = buffer.toString();
 
 const baseBookDirPath = path.resolve(__dirname, "../../../data/books/");
 const baseNotesDirPath = path.resolve(__dirname, "../../../notes/");
-// const notesDirPath = `${baseNotesDirPath}/${dirName}`;
+const rootDirPath = path.resolve(__dirname, "../../../");
 
 const dirArray = fs.readdirSync(baseBookDirPath);
-
+let textData = "";
 for (let i = 0; i < dirArray.length; i++) {
   const dir = dirArray[i];
   if (dir == "README.md") continue;
   const bookJsonDirPath = `${baseBookDirPath}/${dir}/books.json`;
   const json = JSON.parse(fs.readFileSync(bookJsonDirPath));
   json.forEach((obj) => {
-    console.log(createCheckBoxText(obj));
+    textData += `${createCheckBoxText(obj)}\r`;
   });
 }
+
+fs.writeFileSync(`${rootDirPath}/README.md`, textData);
