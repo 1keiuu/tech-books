7d6e5262a5c782df3f31493cb510fb80bddeaf83
1keiuu <ikkei12.h@gmail.com>
2021-04-23 21:29:40 +0900

    fix

---
+++ app/scripts/updateReadme/templateUtility.ts
@@ -0,0 +1,11 @@
+exports.createCheckBoxText = (obj: Book, genreName: string) => {
+  let text = "";
+  if (obj.isDone) text = "- [x]";
+  else text = "- [ ]";
+  const noteLink = `https://github.com/1keiuu/tech-books/tree/main/notes/${genreName}/${obj.slug}`;
+  return `${text} [${obj.title}](${obj.amazonLink}) / [memo](${noteLink})`;
+};
+
+exports.createSubTitle = (title: string) => {
+  return `**${title}**`;
+};
