83a5d4a517e49b44283012da0a320b8bc45402da
1keiuu <ikkei12.h@gmail.com>
2021-04-28 22:01:04 +0900

    add github actions

--- src/spec/bin/utils/file.test.ts
+++ src/spec/bin/utils/file.test.ts
@@ -1,23 +1,28 @@
 import { generateSettingsJson } from "../../../bin/utils/file";
 
-test("function 'generateSettingsJson' returns perfect data.", () => {
-  const data = {
-    id: 1,
-    slug: "test",
-    name: "テスト",
-  };
-  expect(generateSettingsJson(data.id, data.slug, data.name)).toStrictEqual(
-    data
-  );
-});
-
-test("function 'generateSettingsJson' returns 'unset' data.", () => {
-  const data = {
-    id: 1,
-  };
-  expect(generateSettingsJson(data.id)).toStrictEqual({
-    id: 1,
-    slug: "unset",
-    name: "未設定",
+describe("function 'generateSettingsJson' ", () => {
+  test("returns perfect data.", () => {
+    const data = {
+      id: 1,
+      slug: "test",
+      name: "テスト",
+    };
+    expect(generateSettingsJson(data.id, data.slug, data.name)).toStrictEqual(
+      data
+    );
   });
+
+  test("returns 'unset' data.", () => {
+    const data = {
+      id: 1,
+    };
+    expect(generateSettingsJson(data.id)).toStrictEqual({
+      id: 1,
+      slug: "unset",
+      name: "未設定",
+    });
+  });
+});
+test("", () => {
+  console.log(jest.mock("fs"));
 });
